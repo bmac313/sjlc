89382820451ec7b4e89c5202dae98d30f65a5f04
Brendan <mcb0193@comcast.net>
2018-02-28 18:03:44 -0600

    Create storage exception classes

---
+++ src/main/java/org/sjlchatham/sjlcweb/storage/StorageException.java
@@ -0,0 +1,13 @@
+package org.sjlchatham.sjlcweb.storage;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
