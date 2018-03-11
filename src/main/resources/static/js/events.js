2bcd501330eda787b742e0745909514040823702
Brendan <mcb0193@comcast.net>
2018-03-11 16:26:45 -0500

    Clean up jQuery

--- src/main/resources/static/js/events.js
+++ src/main/resources/static/js/events.js
@@ -1,75 +1,56 @@
 $(document).ready(function() {
 
     /* .nav-boxes events*/
-    var aboutOriginal = "<img src='/img/community_links.png'>";
-
-    var aboutMouseOver = "<ul class='block-list'>" +
-                         "<li><a href='/about/staff'>Staff</a></li>" +
-                         "<li><a href='/about/beliefs'>What We Believe</a></li>" +
-                         "<li><a href='/about/history'>Church History</a></li>" +
-                         "</ul>";
-
-    var preschoolOriginal = "<img src='/img/preschool.png'>";
-
-    var preschoolMouseOver = "<div class='inner-nav-box'>" +
-                             "<a href='/preschool' class='btn-primary btn-lg'>Learn More</a>" +
-                             "</div>";
-
-    var calendarOriginal = "<img src='/img/calendar.png'>";
-
-    var calendarMouseOver = "<div class='inner-nav-box'>" +
-                            "<a href='/calendar' class='btn-primary btn-lg'>Learn More</a>" +
-                            "</div>";
-
-    var sermonsOriginal = "<img src='/img/ministries.png'>";
-
-    var sermonsMouseOver = "<ul class='block-list'>" +
-                           "<li><a href='/downloads/sermons'>Sermons</a></li>" +
-                           "<li><a href='/downloads/newsletter'>Newsletter</a></li>" +
-                           "</ul>";
-
 
     $('#about-us').bind({
         mouseenter: function() {
             $('#about-us').addClass('nav-box-active');
-            $('#about-us > img').replaceWith(aboutMouseOver);
+            $('#about-us > img').addClass('hidden');
+            $('#about-us > .block-list').removeClass('hidden');
         },
         mouseleave: function() {
             $('#about-us').removeClass('nav-box-active');
-            $('#about-us > .block-list').replaceWith(aboutOriginal);
+            $('#about-us > img').removeClass('hidden');
+            $('#about-us > .block-list').addClass('hidden');
         }
     });
 
     $('#preschool').bind({
         mouseenter: function() {
             $('#preschool').addClass('nav-box-active');
-            $('#preschool > img').replaceWith(preschoolMouseOver);
+            $('#preschool > img').addClass('hidden');
+            $('#preschool > .block-list').removeClass('hidden');
         },
         mouseleave: function() {
             $('#preschool').removeClass('nav-box-active');
-            $('#preschool > .inner-nav-box').replaceWith(preschoolOriginal);
+            $('#preschool > img').removeClass('hidden');
+            $('#preschool > .block-list').addClass('hidden');
         }
     });
 
     $('#calendar').bind({
         mouseenter: function() {
             $('#calendar').addClass('nav-box-active');
-            $('#calendar > img').replaceWith(calendarMouseOver);
+            $('#calendar > img').addClass('hidden');
+            $('#calendar > .inner-nav-box').removeClass('hidden');
         },
         mouseleave: function() {
             $('#calendar').removeClass('nav-box-active');
-            $('#calendar > .inner-nav-box').replaceWith(calendarOriginal);
+            $('#calendar > img').removeClass('hidden');
+            $('#calendar > .inner-nav-box').addClass('hidden');
         }
     });
 
     $('#sermons-newsletter').bind({
         mouseenter: function() {
             $('#sermons-newsletter').addClass('nav-box-active');
-            $('#sermons-newsletter > img').replaceWith(sermonsMouseOver);
+            $('#sermons-newsletter > img').addClass('hidden');
+            $('#sermons-newsletter > .block-list').removeClass('hidden');
         },
         mouseleave: function() {
             $('#sermons-newsletter').removeClass('nav-box-active');
-            $('#sermons-newsletter > .block-list').replaceWith(sermonsOriginal);
+            $('#sermons-newsletter > img').removeClass('hidden');
+            $('#sermons-newsletter > .block-list').addClass('hidden');
         }
     });
 
