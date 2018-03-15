b33fe80362f2c2e910cd7653f0c54dcfa265e0cf
Brendan <mcb0193@comcast.net>
2018-03-14 20:26:02 -0500

    Add request mappings

--- src/main/java/org/sjlchatham/sjlcweb/controllers/UserController.java
+++ src/main/java/org/sjlchatham/sjlcweb/controllers/UserController.java
@@ -1,16 +1,32 @@
 package org.sjlchatham.sjlcweb.controllers;
 
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
 
-    @RequestMapping("login")
+    @RequestMapping(value = "login", method = RequestMethod.GET)
     public String showLoginPage(Model model) {
+        model.addAttribute("title", "Log In - St. John's Lutheran Church");
+        model.addAttribute("header", "Log In");
         return "users/login";
+    }
+
+    @RequestMapping(value = "login-error", method = RequestMethod.GET)
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        model.addAttribute("title", "Log In - St. John's Lutheran Church");
+        model.addAttribute("header", "Log In");
+        return "users/login";
+    }
+
+    @RequestMapping(value = "admin", method = RequestMethod.GET)
+    public String showAdminPage (ModelMap model) {
+        return "users/admin";
     }
 
 }
