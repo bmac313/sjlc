c40df6fae32f179b2a22b1cd880a67b8878be41d
Brendan <mcb0193@comcast.net>
2018-03-19 19:37:26 -0500

    Edits to User class

--- src/main/java/org/sjlchatham/sjlcweb/models/User.java
+++ src/main/java/org/sjlchatham/sjlcweb/models/User.java
@@ -1,12 +1,13 @@
 package org.sjlchatham.sjlcweb.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
+@Table(name = "USERS")
 public class User {
 
     @Id
@@ -15,12 +16,25 @@ public class User {
 
     @NotNull
     @Size(min = 1, message = "Please enter a username.")
-    private String name;
+    private String username;
 
-    public User(){}
+    @NotNull
+    @Size(min = 5, message = "Passwords must be at least 5 characters long.")
+    private String password;
 
-    public User(String name) {
-        this.name = name;
+    private boolean enabled;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    private Set<Authorities> authorities = new HashSet<>();
+
+    public User(){
+        this.enabled = true;
+    }
+
+    public User(String username, String password, boolean enabled) {
+        this();
+        this.username = username;
+        this.password = password;
     }
 
     public int getId() {
@@ -28,10 +42,42 @@ public class User {
     }
 
     public String getName() {
-        return name;
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Set<Authorities> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Set<Authorities> authorities) {
+        this.authorities = authorities;
     }
 }
