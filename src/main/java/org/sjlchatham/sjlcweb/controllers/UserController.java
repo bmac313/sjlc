e2c9afb8354e8366f70902423cef47f25ce2c257
Brendan <mcb0193@comcast.net>
2018-03-22 18:17:45 -0500

    Add basic sign-up processing code

--- src/main/java/org/sjlchatham/sjlcweb/controllers/UserController.java
+++ src/main/java/org/sjlchatham/sjlcweb/controllers/UserController.java
@@ -1,13 +1,24 @@
 package org.sjlchatham.sjlcweb.controllers;
 
+import org.sjlchatham.sjlcweb.data.UserDao;
+import org.sjlchatham.sjlcweb.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.validation.Valid;
+import java.security.Principal;
+
 @Controller
 public class UserController {
+
+    @Autowired
+    private UserDao userDao;
 
     @RequestMapping(value = "login", method = RequestMethod.GET)
     public String showLoginPage(Model model) {
@@ -24,8 +35,34 @@ public class UserController {
         return "users/login";
     }
 
+    @RequestMapping(value = "signup", method = RequestMethod.GET)
+    public String showSignUpPage(Model model) {
+        model.addAttribute(new User());
+        model.addAttribute("title", "Create an Account - St. John's Lutheran Church");
+        model.addAttribute("header", "Create an Account");
+
+        return "users/signup";
+    }
+
+    @RequestMapping(value = "signup", method = RequestMethod.POST)
+    public String signUpUser(@Valid @ModelAttribute User newUser,
+                             Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Create an Account - St. John's Lutheran Church");
+            model.addAttribute("header", "Create an Account");
+
+            return "users/signup";
+        }
+
+        userDao.save(newUser);
+
+        return "redirect:";
+
+    }
+
     @RequestMapping(value = "admin", method = RequestMethod.GET)
-    public String showAdminPage (ModelMap model) {
+    public String showAdminPage (ModelMap model, Principal principal) {
+        String loggedInUser = principal.getName(); // Username of the currently logged-in user / who is sending this request.
         return "users/admin";
     }
 
