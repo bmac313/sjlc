e739d919bd983eea2454a15a5b535ebd9f2c449d
Brendan <mcb0193@comcast.net>
2018-03-28 17:51:41 -0500

    Create BcryptTest

---
+++ src/test/java/org/sjlchatham/sjlcweb/BcryptTest.java
@@ -0,0 +1,15 @@
+package org.sjlchatham.sjlcweb;
+
+import org.junit.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class BcryptTest {
+
+    @Test
+    public void bcryptTest() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encodedPw =  encoder.encode("test123");
+        System.out.println(encodedPw);
+    }
+
+}
