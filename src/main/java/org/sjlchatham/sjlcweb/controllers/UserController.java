f0bf69e763ea3df4fef68b87656b3361a1101684
Brendan <mcb0193@comcast.net>
2018-04-08 18:03:05 -0500

    Add logout processing

--- src/main/java/org/sjlchatham/sjlcweb/controllers/UserController.java
+++ src/main/java/org/sjlchatham/sjlcweb/controllers/UserController.java
@@ -6,6 +6,9 @@ import org.sjlchatham.sjlcweb.models.Authorities;
 import org.sjlchatham.sjlcweb.models.User;
 import org.sjlchatham.sjlcweb.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -15,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.security.Principal;
 
@@ -86,6 +91,16 @@ public class UserController {
     public String adminService (ModelMap model, Principal principal) {
         adminService.performSomeAdminService();
         return "users/admin";
+    }
+
+    @RequestMapping(value = "logout", method = RequestMethod.GET)
+    public String logOutUser(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+
+        return "redirect:/login?logout";
     }
 
 }
