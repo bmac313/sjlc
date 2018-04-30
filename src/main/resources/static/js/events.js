d7a0c0f420695b48611a2cd0ef9ba96b0b2a273c
Brendan <mcb0193@comcast.net>
2018-04-30 18:17:48 -0500

    Optimize nav box system for mobile.

--- src/main/resources/static/js/events.js
+++ src/main/resources/static/js/events.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
+    initEvents();
+});
 
+$(window).resize(function() {
+    rebindNavBoxEvents();
+});
+
+var initEvents = function() {
+    bindNavBarEvents();
+    bindNavBoxEvents();
+}
+
+var bindNavBarEvents = function() {
     /* navbar events */
     if (document.getElementById("navbar")) { // If the navbar is present in the document
         window.onscroll = function() {stickNavBar()};
@@ -14,60 +26,103 @@ $(document).ready(function() {
             navbar.classList.remove("sticky");
         }
     }
+}
 
-
+var bindNavBoxEvents = function() {
     /* .nav-boxes events */
+    if ($(window).width() >= 751) {
+        $('#mobile-nav-instructions').addClass('hidden');
+        $('#about-us').bind({
+                mouseenter: function() {
+                    $('#about-us').addClass('nav-box-active');
+                    $('#about-us > img').addClass('hidden');
+                    $('#about-us > .block-list').removeClass('hidden');
+                },
+                mouseleave: function() {
+                    $('#about-us').removeClass('nav-box-active');
+                    $('#about-us > img').removeClass('hidden');
+                    $('#about-us > .block-list').addClass('hidden');
+                }
+            });
 
-    $('#about-us').bind({
-        mouseenter: function() {
-            $('#about-us').addClass('nav-box-active');
-            $('#about-us > img').addClass('hidden');
-            $('#about-us > .block-list').removeClass('hidden');
-        },
-        mouseleave: function() {
-            $('#about-us').removeClass('nav-box-active');
-            $('#about-us > img').removeClass('hidden');
-            $('#about-us > .block-list').addClass('hidden');
-        }
-    });
+        $('#preschool').bind({
+            mouseenter: function() {
+                $('#preschool').addClass('nav-box-active');
+                $('#preschool > img').addClass('hidden');
+                $('#preschool > .block-list').removeClass('hidden');
+            },
+            mouseleave: function() {
+                $('#preschool').removeClass('nav-box-active');
+                $('#preschool > img').removeClass('hidden');
+                $('#preschool > .block-list').addClass('hidden');
+            }
+        });
 
-    $('#preschool').bind({
-        mouseenter: function() {
-            $('#preschool').addClass('nav-box-active');
-            $('#preschool > img').addClass('hidden');
-            $('#preschool > .block-list').removeClass('hidden');
-        },
-        mouseleave: function() {
-            $('#preschool').removeClass('nav-box-active');
-            $('#preschool > img').removeClass('hidden');
-            $('#preschool > .block-list').addClass('hidden');
-        }
-    });
+        $('#calendar').bind({
+            mouseenter: function() {
+                $('#calendar').addClass('nav-box-active');
+                $('#calendar > img').addClass('hidden');
+                $('#calendar > .block-list').removeClass('hidden');
+            },
+            mouseleave: function() {
+                $('#calendar').removeClass('nav-box-active');
+                $('#calendar > img').removeClass('hidden');
+                $('#calendar > .block-list').addClass('hidden');
+            }
+        });
 
-    $('#calendar').bind({
-        mouseenter: function() {
-            $('#calendar').addClass('nav-box-active');
-            $('#calendar > img').addClass('hidden');
-            $('#calendar > .block-list').removeClass('hidden');
-        },
-        mouseleave: function() {
-            $('#calendar').removeClass('nav-box-active');
-            $('#calendar > img').removeClass('hidden');
-            $('#calendar > .block-list').addClass('hidden');
-        }
-    });
+        $('#sermons-newsletter').bind({
+            mouseenter: function() {
+                $('#sermons-newsletter').addClass('nav-box-active');
+                $('#sermons-newsletter > img').addClass('hidden');
+                $('#sermons-newsletter > .block-list').removeClass('hidden');
+            },
+            mouseleave: function() {
+                $('#sermons-newsletter').removeClass('nav-box-active');
+                $('#sermons-newsletter > img').removeClass('hidden');
+                $('#sermons-newsletter > .block-list').addClass('hidden');
+            }
+        });
+    } else {
+        $('#mobile-nav-instructions').removeClass('hidden');
+        $('#about-us').bind({
+            click: function() {
+                $('#about-us').toggleClass('nav-box-active');
+                $('#about-us > img').toggleClass('hidden');
+                $('#about-us > .block-list').toggleClass('hidden');
+            }
+        });
 
-    $('#sermons-newsletter').bind({
-        mouseenter: function() {
-            $('#sermons-newsletter').addClass('nav-box-active');
-            $('#sermons-newsletter > img').addClass('hidden');
-            $('#sermons-newsletter > .block-list').removeClass('hidden');
-        },
-        mouseleave: function() {
-            $('#sermons-newsletter').removeClass('nav-box-active');
-            $('#sermons-newsletter > img').removeClass('hidden');
-            $('#sermons-newsletter > .block-list').addClass('hidden');
-        }
-    });
+        $('#preschool').bind({
+            click: function() {
+                $('#preschool').toggleClass('nav-box-active');
+                $('#preschool > img').toggleClass('hidden');
+                $('#preschool > .block-list').toggleClass('hidden');
+            }
+        });
 
-});
+        $('#calendar').bind({
+            click: function() {
+                $('#calendar').toggleClass('nav-box-active');
+                $('#calendar > img').toggleClass('hidden');
+                $('#calendar > .block-list').toggleClass('hidden');
+            }
+        });
+
+        $('#sermons-newsletter').bind({
+            click: function() {
+                $('#sermons-newsletter').toggleClass('nav-box-active');
+                $('#sermons-newsletter > img').toggleClass('hidden');
+                $('#sermons-newsletter > .block-list').toggleClass('hidden');
+            }
+        });
+    }
+};
+
+var rebindNavBoxEvents = function() {
+    $('#about-us').unbind();
+    $('#preschool').unbind();
+    $('#calendar').unbind();
+    $('#sermons-newsletter').unbind();
+    bindNavBoxEvents();
+};
