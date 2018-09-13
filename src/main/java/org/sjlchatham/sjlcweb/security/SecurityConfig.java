2a641f41cd35441a24098b0f637bdbd2dec84401
Brendan <mcb0193@comcast.net>
2018-09-13 11:18:36 -0500

    Add new auth paths

--- src/main/java/org/sjlchatham/sjlcweb/security/SecurityConfig.java
+++ src/main/java/org/sjlchatham/sjlcweb/security/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                             "/news/viewpost/*",
                             "/links",
                             "/downloads/**").permitAll()
-                    .antMatchers("/news/new-post", "/news/editpost/**", "/news/deletepost", "/news/deletepost/**" ).hasRole("ADMIN")
+                    .antMatchers("/news/new-post", "/news/editpost/**", "/news/deletepost", "/news/deletepost/**", "/changepass", "/changepass/**" ).hasRole("ADMIN")
                         .anyRequest().authenticated()
                         .and()
                 .formLogin()
