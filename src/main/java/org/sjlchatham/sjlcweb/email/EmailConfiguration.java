7042babbfcfdcd89e4a766fb504e98b8eb25cc88
Brendan <mcb0193@comcast.net>
2018-10-04 12:50:47 -0500

    Create class EmailConfiguration

---
+++ src/main/java/org/sjlchatham/sjlcweb/email/EmailConfiguration.java
@@ -0,0 +1,34 @@
+package org.sjlchatham.sjlcweb.email;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfiguration {
+
+    @Bean(name = "mailSender")
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        /* TODO: Set up test gmail server for testing purposes, replace below
+         *       properties with server login credentials.
+         */
+        //mailSender.setUsername("usernamegoeshere");
+        //mailSender.setPassword("passwordgoeshere");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
+}
