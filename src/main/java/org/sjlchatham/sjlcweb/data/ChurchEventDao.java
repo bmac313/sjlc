9cda7d141e3a9f79a5f575128f28a8fcecd78a8b
Brendan <mcb0193@comcast.net>
2020-05-20 19:10:43 -0500

    Make ChurchEventDao a PagingAndSortingRepository

--- src/main/java/org/sjlchatham/sjlcweb/data/ChurchEventDao.java
+++ src/main/java/org/sjlchatham/sjlcweb/data/ChurchEventDao.java
@@ -2,11 +2,14 @@ package org.sjlchatham.sjlcweb.data;
 
 import org.sjlchatham.sjlcweb.models.ChurchEvent;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 
 @Repository
 @Transactional
-public interface ChurchEventDao extends CrudRepository<ChurchEvent, Integer>{
+public interface ChurchEventDao extends PagingAndSortingRepository<ChurchEvent, Integer> {
+    ChurchEvent findFirstByOrderByIdDesc();
+    ChurchEvent findFirstByOrderByDateTimeDesc();
 }
