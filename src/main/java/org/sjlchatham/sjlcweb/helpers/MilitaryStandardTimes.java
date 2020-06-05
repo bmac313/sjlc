cbdf9ad551abbaac189dbe547179b09c36f2551f
Brendan <mcb0193@comcast.net>
2020-06-04 21:16:31 -0500

    Create class MilitaryStandardTimes

---
+++ src/main/java/org/sjlchatham/sjlcweb/helpers/MilitaryStandardTimes.java
@@ -0,0 +1,13 @@
+package org.sjlchatham.sjlcweb.helpers;
+
+import java.util.HashMap;
+
+public class MilitaryStandardTimes {
+
+    private HashMap<String, String> militaryTimesAndEquivalents;
+
+    public MilitaryStandardTimes(HashMap<String, String> militaryTimesAndEquivalents) {
+        //TODO: This should contain military hours and their equivalent standard hours (e.g. 13:00 -> 1:00 PM)
+        this.militaryTimesAndEquivalents = militaryTimesAndEquivalents;
+    }
+}
