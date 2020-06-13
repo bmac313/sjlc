daddae8127fff0bbe5054d3179b68c695b9dc7b5
Brendan <mcb0193@comcast.net>
2020-06-13 17:46:51 -0500

    Create helper class StringOps

---
+++ src/main/java/org/sjlchatham/sjlcweb/helpers/StringOps.java
@@ -0,0 +1,12 @@
+package org.sjlchatham.sjlcweb.helpers;
+
+// This class contains helper methods regarding String operations
+public class StringOps {
+
+    public StringOps() {}
+
+    public static boolean isNullOrEmpty(String str) {
+        return (str==null || str.isEmpty());
+    }
+
+}
